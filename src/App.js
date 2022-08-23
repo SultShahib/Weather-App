ea513599863976317e83aa912207c65c16c30440
sultshahib1@gmail.com <89592909+SultShahib@users.noreply.github.com>
2022-08-23 17:25:18 +0800

    Added Realtime functioni

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import Header from "./components/Nav/Nav";
+import Realtime from "./components/Realtime";
+
+export const API = "http://api.weatherapi.com/v1";
+export const KEY = "f7139efdcaea4fc9829230426222208";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <Realtime />
+    </>
   );
 }
 
---
+++ src/components/Realtime.js
@@ -0,0 +1,15 @@
+import MediaCard from "./Card/Card";
+import { API, KEY } from "../App";
+import axios from "axios";
+
+const Realtime = () => {
+  const getRealTimeWeather = async (api, key) => {};
+
+  return (
+    <>
+      <MediaCard />
+    </>
+  );
+};
+
+export default Realtime;
